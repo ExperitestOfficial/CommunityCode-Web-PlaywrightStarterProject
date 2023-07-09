1b05bd8b4005721d9d0bb9fa1eef0573112440e1
karincarmely <kcarmely@digital.ai>
2023-07-09 11:37:34 +0300

    add webkit browser, remove unnecessary lines in conf

--- playwrightProject/playwright.config.js
+++ playwrightProject/playwright.config.js
@@ -1,12 +1,5 @@
-// @ts-check
+
 const { devices } = require('@playwright/test');
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -39,7 +32,7 @@ const config = {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Add data-auto testIdAttribute to be able to find elements in the demo website */
 	testIdAttribute: 'data-auto',
 	video: 'on',
   },
@@ -66,44 +59,10 @@ const config = {
         // ...devices['Desktop Safari'],
       // },
     // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
  // ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 };
 
 module.exports = config;
